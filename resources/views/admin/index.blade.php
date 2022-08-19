7c5f85f6b99675231db1c19d9cc333269c9da2fe
Hung Vu Manh <hung.vu2@ntq-solution.com.vn>
2022-08-19 08:38:23 +0700

    add management admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+
+    <link rel="canonical" href="https://demo-basic.adminkit.io/" />
+
+    <title>Admin</title>
+
+    <link href="{{ mix('/css/admin.css') }}" rel="stylesheet">
+    <link href="{{ mix('/css/css.css') }}" rel="stylesheet">
+    <script src="{{ mix('/js/admin.js') }}"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- include summernote css/js -->
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <!-- Left Slide Bar -->
+        @include('admin.sidebar')
+
+        <!-- Main -->
+        <div class="main">
+            @include('admin.navbar')
+
+            <main class="content">
+                @yield('content')
+            </main>
+
+            <footer class="footer">
+                @include('admin.footer')
+            </footer>
+        </div>
+    </div>
+
+    <script>
+    $(document).ready(function() {
+        $('#summernote').summernote({
+            placeholder: 'Hello stand alone ui',
+            height: 120,
+            toolbar: [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['fontname', ['fontname']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['view', ['fullscreen', 'codeview', 'help']]
+            ]
+        });
+    });
+    </script>
+</body>
