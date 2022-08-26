a5e872c1ae693da759f8540739642f72910f3173
Hung Vu Manh <hung.vu2@ntq-solution.com.vn>
2022-08-26 10:52:18 +0700

    relationship model

---
+++ app/Models/ProductColorModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductColorModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product_color_models';
+
+    public $fillable = [
+        'name',
+        'color_code'
+    ];
+
+    public function component()
+    {
+        return $this->belongsTo(ProductComponentModel::class, 'color', 'id');
+    }
+}
--- database/migrations/2022_08_26_031705_create_product_component_models_table.php
+++ database/migrations/2022_08_26_031705_create_product_component_models_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductModelsTable extends Migration
+class CreateProductComponentModelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreateProductModelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('product_models', function (Blueprint $table) {
+        Schema::create('product_component_models', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('type')->nullable();
-            $table->mediumText('description')->nullable();
+            $table->string('product_id');
+            $table->string('memory');
+            $table->string('color_id');
+            $table->string('amount');
+            $table->string('price');
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -29,6 +32,6 @@ class CreateProductModelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product_models');
+        Schema::dropIfExists('product_component_models');
     }
 }
