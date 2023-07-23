d792dc98e0c604ee965809435e3ae1d4555639ee
hungvu <hungvumanh11o23@gmail.com>
2023-07-23 15:23:58 +0700

    update: api momo

--- app/Services/MomoService.php
+++ app/Services/MomoService.php
@@ -16,40 +16,57 @@ class MomoService extends BaseService
 
     public function createPayment($orderId, $total)
     {
-        $partnerCode = MOMO_PARTNER_CODE;
-        $accessKey = MOMO_ACCESS_KEY;
-        $secretKey = MOMO_SECRET_KEY;
-        $orderInfo = 'Thanh toán qua MoMo';
-        $amount = $total;
-        $orderId = $orderId."";
-        $returnUrl = route(RESULT_PAYMENT_MOMO);
-        $notifyUrl = MOMO_NOTIFY_URL;
-        $bankCode = MOMO_BANK_CODE;
-        $requestType = "payWithMoMoATM";
-        $requestId = time()."";
-        $extraData = "";
-        $endpoint = "https://test-payment.momo.vn/gw_payment/transactionProcessor";
+        try {
+            $partnerCode = MOMO_PARTNER_CODE;
+            $accessKey = MOMO_ACCESS_KEY;
+            $secretKey = MOMO_SECRET_KEY;
+            $orderInfo = 'Thanh toán qua MoMo';
+            $amount = $total;
+            $orderId = $orderId . "";
+            $returnUrl = route(RESULT_PAYMENT_MOMO);
+            $notifyUrl = MOMO_NOTIFY_URL;
+            $requestType = "payWithATM";
+            $requestId = time() . "";
+            $extraData = "";
+            $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
+            $partnerClientId = 'hungvumanh11o23@gmail.com';
 
-        $rawHash = "partnerCode=".$partnerCode."&accessKey=".$accessKey."&requestId=".$requestId."&bankCode=".$bankCode."&amount=".$amount."&orderId=".$orderId."&orderInfo=".$orderInfo."&returnUrl=".$returnUrl."&notifyUrl=".$notifyUrl."&extraData=".$extraData."&requestType=".$requestType;
-        $signature = hash_hmac("sha256", $rawHash, $secretKey);
+            $rawHash = "accessKey=" . $accessKey .
+                "&amount=" . $amount .
+                "&extraData=" . $extraData .
+                "&ipnUrl=" . $returnUrl .
+                "&orderId=" . $orderId .
+                "&orderInfo=" . $orderInfo .
+                "&partnerClientId=" . $partnerClientId .
+                "&partnerCode=" . $partnerCode .
+                "&redirectUrl=" . $returnUrl .
+                "&requestId=" . $requestId .
+                "&requestType=" . $requestType;
+            $signature = hash_hmac("sha256", $rawHash, $secretKey);
 
-        $data =  array('partnerCode' => $partnerCode,
-            'accessKey' => $accessKey,
-            'requestId' => $requestId,
-            'amount' => $amount,
-            'orderId' => $orderId,
-            'orderInfo' => $orderInfo,
-            'returnUrl' => $returnUrl,
-            'bankCode' => $bankCode,
-            'notifyUrl' => $notifyUrl,
-            'extraData' => $extraData,
-            'requestType' => $requestType,
-            'signature' => $signature);
+            $data = array(
+                'partnerCode' => $partnerCode,
+                'accessKey' => $accessKey,
+                'requestId' => $requestId,
+                'amount' => $amount,
+                'orderId' => $orderId,
+                'orderInfo' => $orderInfo,
+                'redirectUrl' => $returnUrl,
+                'ipnUrl' => $returnUrl,
+                'extraData' => $extraData,
+                'requestType' => $requestType,
+                'signature' => $signature,
+                'partnerClientId' => $partnerClientId
+            );
 
-        //send api to momo
-        $result = $this->execPostRequest($endpoint, json_encode($data));
-        $jsonResult = json_decode($result,true);
+            //send api to momo
+            $result = $this->execPostRequest($endpoint, json_encode($data));
 
-        return $jsonResult['payUrl'];
+            $jsonResult = json_decode($result, true);
+            dd($jsonResult);
+            return $jsonResult['payUrl'];
+        } catch (\Exception $e) {
+            return route(STORE_CART);
+        }
     }
 }
