c064984c228ab279a229e81b40f423accb5a9e5b
h0antran <hoantd@coccoc.com>
2024-12-19 11:33:15 +0700

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ image=rg.hoantran.me/do-an/php:$(date +%s)
 sudo docker build -t $image .
 sudo docker push $image
 
-cd ~/project/K8s-manifest/do-an; kustomize edit set $image
+cd ~/project/K8s-manifests/do-an; kustomize edit set image $image
 
 git add .
 git commit -m "Update do-an image to $image"
 git push
 
 ssh-agent && eval "$(ssh-agent)" && ssh-add ~/.ssh/id_ed25519 && ssh -A hoantd-k0 'cd K8s-manifests && git pull'
-ssh hoantd-k0 kubectl apply -k K8s-manifests/do-an
+ssh hoantd-k0 kubectl apply -k K8s-manifests/do-an/
 
 
 echo "Deployed"
